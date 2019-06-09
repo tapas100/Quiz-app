3e9a77938539c3c9407e3e42304713b7429c3223
Refixd <tapas.refixd>
2019-06-10 02:26:08 +0530

    deployment 4

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/<name-of-app>'));
 
-app.get('/*', function(req,res) {
+app.get('*', function (req, res) {
+    const index = path.join(__dirname, 'build', 'index.html');
+    res.sendFile(index);
+  });{
     
 res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
 });
